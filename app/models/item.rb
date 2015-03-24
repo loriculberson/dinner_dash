189b77df174a324748b36991fde952135419251f
Lori Culberson <bigskybandit@sbcglobal.net>
2015-03-24 09:04:00 -0600

    deleted empty lines

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   validates :description, :price, presence: true
   validates :title, presence: true, uniqueness: {case_sensitive: false}
-  validates :price, numericality: { greater_than: 0}
+  validates :price, numericality: {greater_than: 0}
   belongs_to :item_status
   has_many :item_categories
   has_many :order_items
